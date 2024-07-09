4753ca94ab9220da30a42a5e1b7a39d1322db470
ArthurAD <arthurdobrya@gmail.com>
2024-07-09 13:33:35 +0300

    Lobby,Relay,simple game

---
+++ Assets/LobbyTutorial/Scripts/TestRelay.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+public class TestRelay : MonoBehaviour
+{
+    public static TestRelay Instance { get; private set; }
+	    
+    private void Awake() {
+        Instance = this;
+    }
+
+    [ServerRpc]
+    public async void JoinRelay(String joinCode)
+    {
+        try
+        {
+            Debug.Log("Joining with code: " + joinCode);
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [ServerRpc]
+    public async Task<string> CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3); //number of players
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            Debug.Log(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+
+            return joinCode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+            return null;
+        }
+    }
+}
